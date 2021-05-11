2238636d3adc8ff664a99f606eed089aada6d26a
MigPalSer <migpalser@protonmail.ch>
2021-05-11 12:04:13 +0200

    Start the project

---
+++ src/main/java/com/migpalser/mylitecrm/MyLiteCRM/MyLiteCrmApplication.java
@@ -0,0 +1,13 @@
+package com.migpalser.mylitecrm.MyLiteCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLiteCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLiteCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migpalser/mylitecrm/MyLiteCRM/MyLiteCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migpalser.mylitecrm.MyLiteCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLiteCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
