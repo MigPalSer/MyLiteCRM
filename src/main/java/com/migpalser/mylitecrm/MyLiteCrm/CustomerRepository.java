0935504872809281b5541f63504621f66ab33614
MigPalSer <migpalser@protonmail.ch>
2021-05-22 07:37:34 +0200

    REST implementation

--- src/main/java/com/migpalser/mylitecrm/MyLiteCrm/CustomerRepository.java
+++ src/main/java/com/migpalser/mylitecrm/MyLiteCrm/CustomerRepository.java
@@ -3,7 +3,11 @@ package com.migpalser.mylitecrm.MyLiteCrm;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="customers")
 public interface CustomerRepository extends CrudRepository<Customer, String> {
 
 	public List<Customer> findByName(String name);
@@ -12,5 +16,9 @@ public interface CustomerRepository extends CrudRepository<Customer, String> {
 	public List<Customer> findByPhonenumber(int phonenumber);
 	public List<Customer> findByNameAndSurnames(String name, String surnames);
 	public List<Customer> findBySurnamesContaining(String surnames);
+	//public List<Customer> findByPhonenumberNull(int phonenumber);
+	//public List<Customer> findByNameNull(String name);
+	//public List<Customer> findByPhonenumberIn(int phonenumber);
+
 
 }
